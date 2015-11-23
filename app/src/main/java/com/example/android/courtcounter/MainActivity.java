fd73640d8bcbab64db262e2543bed896f351cec1
Justin Raczak <jraczak@gmail.com>
2015-11-23 14:15:49 -0800

    Initial commit to use for XTC testing

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int teamAScore = 0;
+    int teamBScore = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Adds 3 points to Team A score
+     */
+    public void addThreePointsA(View view) {
+        teamAScore = teamAScore + 3;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Adds 2 points to Team A score
+     */
+    public void addTwoPointsA(View view) {
+        teamAScore = teamAScore + 2;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Adds 1 point to Team A score
+     */
+    public void addOnePointA(View view) {
+        teamAScore = teamAScore + 1;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Adds 3 points to Team B score
+     */
+    public void addThreePointsB(View view) {
+        teamBScore = teamBScore + 3;
+        displayForTeamB(teamBScore);
+    }
+
+    /**
+     * Adds 2 points to Team A score
+     */
+    public void addTwoPointsB(View view) {
+        teamBScore = teamBScore + 2;
+        displayForTeamB(teamBScore);
+    }
+
+    /**
+     * Adds 1 point to Team A score
+     */
+    public void addOnePointB(View view) {
+        teamBScore = teamBScore + 1;
+        displayForTeamB(teamBScore);
+    }
+
+    public void resetScores(View view) {
+        teamAScore = 0;
+        teamBScore = 0;
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
+    }
+}
